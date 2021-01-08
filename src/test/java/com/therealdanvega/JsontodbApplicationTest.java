19b3ff45d728f05b7112b55b625bd84206ccaf82
blockedhead <73295213+blockedhead@users.noreply.github.com>
2021-01-08 12:08:43 +0100

    updated test class

---
+++ src/test/java/com/therealdanvega/JsontodbApplicationTest.java
@@ -0,0 +1,21 @@
+package com.therealdanvega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *
+ * @author AHE
+ */
+
+@SpringBootTest
+class JsontodbApplicationTest {
+    
+    public JsontodbApplicationTest() {
+    }
+
+	@Test
+	void contextLoads() {
+	}
+
+}
