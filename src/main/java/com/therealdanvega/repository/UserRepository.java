676ee1fb9d8588d0dbf8c9da02d5a352b0cb3725
blockedhead <73295213+blockedhead@users.noreply.github.com>
2021-01-08 12:07:07 +0100

    Crud -> Jpa - Repository
    Jpa includes Crud but has more functions

--- src/main/java/com/therealdanvega/repository/UserRepository.java
+++ src/main/java/com/therealdanvega/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.therealdanvega.repository;
 
 import com.therealdanvega.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 
 
