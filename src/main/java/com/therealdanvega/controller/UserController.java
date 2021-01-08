c77fa2e30f003c5271671fde373dc2040a75d7aa
blockedhead <73295213+blockedhead@users.noreply.github.com>
2021-01-08 12:06:17 +0100

    Update UserController.java
    Userservice can be final

--- src/main/java/com/therealdanvega/controller/UserController.java
+++ src/main/java/com/therealdanvega/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
